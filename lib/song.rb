1e4ebd16b1913a6df66e4c5bb74aa302fc6ab23a
Joseph Perez <hello@josephdanielperez.com>
2020-08-10 05:35:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,10 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    
-  end
+    title = filename.split(" - ")
+    artist = Artist.find_or_create_by_name(title[0])
+    genre = Genre.find_or_create_by_name(title[2][0..-5])
+    create(title[1], artist, genre)
+  end 
 
 end
