5749b2c6587136da8aedeaf4f3ab6f89203a07ef
Joseph Perez <hello@josephdanielperez.com>
2020-08-10 04:58:53 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(song)
+    new_song = Song.new(song)
+    new_song
+  end
+
+end
