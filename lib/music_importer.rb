698c493fccff4bac4bf86f5b8d2926fb37a66fc3
Joseph Perez <hello@josephdanielperez.com>
2020-08-10 05:33:53 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -6,10 +6,10 @@ class MusicImporter
   end
 
   def files
-    Dir.entries(path).select{ |file| file.end_with?(".mp3") }
+    Dir.entries(path).select{|file| file.end_with?(".mp3")}
   end
 
   def import
-    files.each { |file| Song.create_from_filename(file) }
+    files.each {|file| Song.create_from_filename(file)}
   end
 end
