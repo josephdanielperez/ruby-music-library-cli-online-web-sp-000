8c1d4414a626dd2b5bf01b393bb9dca4e729ca0c
Joseph Perez <hello@josephdanielperez.com>
2020-08-10 05:46:50 +0000

    Automatically backed up by Learn

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -12,6 +12,7 @@ class MusicLibraryController
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
       puts "To play a song in your library, enter 'play song'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
       
       input = gets.chomp
       case input
